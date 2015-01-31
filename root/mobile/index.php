8fdd7a35b6e7a4c292d33dcae05e786f401c8471
rickey29 <rickey29@gmail.com>
2015-01-31 13:58:04 +0800

    1.1.0

---
+++ root/mobile/index.php
@@ -0,0 +1,97 @@
+<?php
+/*
+	project: Mobile phpBB 3.1 (MphpBB31)
+	file:    $phpbb_root_path/mobile/index.php
+	version: 1.1.0
+	author:  Rickey Gu
+	web:     http://flexplat.com
+	email:   rickey29@gmail.com
+*/
+
+if ( !defined('IN_PHPBB') )
+{
+	exit;
+}
+
+
+if ( $this->data['is_bot'] || defined('ADMIN_START') || defined('IN_ADMIN') )
+{
+	return;
+}
+
+if ( $style_id )
+{
+	return;
+}
+
+
+define('IN_MPHPBB31', true);
+
+// detection library
+require($phpbb_root_path . 'mobile/lib/detection.' . $phpEx);
+
+
+global $user, $request;
+
+
+$m_data = array();
+$m_data['user_agent'] = $request->server('HTTP_USER_AGENT');
+$m_data['accept'] = $request->server('HTTP_ACCEPT');
+$m_data['profile'] = $request->server('HTTP_PROFILE');
+$m_data['redirection'] = $request->variable('m-redirection', '');
+$m_data['cookie'] = $request->variable($config['cookie_name'] . '_m-redirection', '', true, \phpbb\request\request_interface::COOKIE);
+
+$m_value = m_get_redirection($m_data);
+
+$m_response = array();
+$m_response['device_platform'] = $m_value[0];
+$m_response['device_grade'] = $m_value[1];
+$m_response['device_system'] = $m_value[2];
+$m_response['echo_page'] = $m_value[3];
+$m_response['set_cookie'] = $m_value[4];
+
+
+if ( !empty($m_response['set_cookie']) )
+{
+	if ( $m_response['set_cookie'] == 'mobile' )
+	{
+		// the cookie expires one year later: 60 * 60 * 24 * 365 = 31,536,000
+		$user->set_cookie('m-redirection', 'mobile', time() + 31536000);
+	}
+	else if ( $m_response['set_cookie'] == 'desktop' )
+	{
+		// the cookie expires one year later: 60 * 60 * 24 * 365 = 31,536,000
+		$user->set_cookie('m-redirection', 'desktop', time() + 31536000);
+	}
+}
+
+if ( !empty($m_response['echo_page']) && $m_response['echo_page'] == 'mobile' )
+{
+	if ( $m_response['device_system'] == 'smartphone' )
+	{
+		define('MPHPBB31', 'jQuery-Mobile Smartphone');
+	}
+	else if ( $m_response['device_system'] == 'tablet' )
+	{
+		define('MPHPBB31', 'jQuery-Mobile Tablet');
+	}
+	else
+	{
+		define('MPHPBB31', 'Feature Phone');
+	}
+
+
+	$sql = 'SELECT style_id
+		FROM ' . STYLES_TABLE . "
+		WHERE style_name = '" . $db->sql_escape(MPHPBB31) . "'";
+	$result = $db->sql_query($sql);
+	$row = $db->sql_fetchrow($result);
+	$db->sql_freeresult($result);
+
+	$style_id = $row['style_id'];
+
+
+	// library
+	include($phpbb_root_path . 'mobile/lib/lib.' . $phpEx);
+}
+?>
---
+++ root/mobile/lib/lib.php
@@ -0,0 +1,89 @@
+<?php
+/*
+	project: Mobile phpBB 3.1 (MphpBB31)
+	file:    $phpbb_root_path/mobile/lib/lib.php
+	version: 1.1.0
+	author:  Rickey Gu
+	web:     http://flexplat.com
+	email:   rickey29@gmail.com
+*/
+
+if ( !defined('IN_PHPBB') )
+{
+	exit;
+}
+
+if ( !defined('IN_MPHPBB31') )
+{
+	exit;
+}
+
+
+function m_update_address_list(&$address_list)
+{
+	if ( empty($address_list) || !is_array($address_list) )
+	{
+		return;
+	}
+
+	$pattern = '#^\s*<a[^>]*>\s*(.*)\s*</a>\s*$#isU';
+	foreach ( $address_list as $key => $value )
+	{
+		if ( preg_match($pattern, $value, $matches) )
+		{
+			$address_list[$key] = $matches[1];
+		}
+	}
+}
+
+function m_update_err($err)
+{
+	$pattern = '#<a[^>]*>\s*(.*)\s*</a>#isU';
+	$err = preg_replace($pattern, '$1', $err);
+
+	return $err;
+}
+
+function m_update_msg_text($msg_text)
+{
+	global $template;
+
+	$pattern = '#<br\s*/>\s*<br\s*/>#isU';
+	$pattern2 = '#^(.*)<a.*\s+href\s*=\s*("|\')([^\\2]*)\\2.*>\s*(.*)\s*</a>(.*)$#isU';
+	foreach ( preg_split($pattern, $msg_text) as $key => $value )
+	{
+		$value = trim($value);
+
+		if ( $key == 0 )
+		{
+			$template->assign_vars(array(
+				'TEXT_HEADING' => $value
+			));
+
+			continue;
+		}
+
+		if ( preg_match($pattern2, $value, $matches) )
+		{
+			$template->assign_block_vars('msg_text_row', array(
+				'TEXT' => $matches[1] . $matches[4] . $matches[5],
+				'U_TEXT' => $matches[3]
+			));
+		}
+		else
+		{
+			$template->assign_block_vars('msg_text_row', array(
+				'TEXT' => $value
+			));
+		}
+	}
+}
+
+function m_update_post_row(&$post_row)
+{
+	$pattern = '#<a[^>]*>\s*(.*)\s*</a>#isU';
+
+	$post_row['L_POST_DELETED_MESSAGE'] = preg_replace($pattern, '$1', $post_row['L_POST_DELETED_MESSAGE']);
+	$post_row['L_IGNORE_POST'] = preg_replace($pattern, '$1', $post_row['L_IGNORE_POST']);
+}
+?>
